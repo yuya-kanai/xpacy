053ded99f0e7d9e2d41bb7f4940405f90d6d11b7
yuya-kanai <yuya-kanai@gmotech.jp>
2019-04-22 15:05:36 +0900

    Add food model and controller

---
+++ database/migrations/2019_04_22_053220_create_foods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->float('price');
+            $table->string('name');
+            $table->string('picture_url');
+            $table->string('url');
+            $table->double('longitude');
+            $table->double('latitude');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
