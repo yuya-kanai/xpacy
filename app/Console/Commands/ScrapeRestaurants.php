014f316543c5dfbc7e7795183cef9c10364bbe8c
yuya-kanai <yuya-kanai@gmotech.jp>
2019-04-23 18:25:34 +0900

    Add scrape for mapurl and price

--- app/Console/Commands/ScrapeRestaurants.php
+++ app/Console/Commands/ScrapeRestaurants.php
@@ -7,6 +7,7 @@ use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\WebDriverExpectedCondition;
 use Facebook\WebDriver\WebDriverBy;
+use Doctrine\DBAL\Driver\OCI8\Driver;
 
 class ScrapeRestaurants extends Command
 {
@@ -69,36 +70,51 @@ class ScrapeRestaurants extends Command
                 )
             );
             $element->click();
+            $current_handle = $this->driver->getWindowHandles();
+            echo("Current : ".count($current_handle)."\r\n");
         }else{
-            echo('Not available : '.$xpath.'¥n');
         }
      
     }
 
+    private function getSrcWithXpath(String $xpath){
+        $xpath_web = WebDriverBy::xpath(
+            $xpath
+        );
+        if( count($this->driver->findElements($xpath_web)) > 0 ) {
+            $element = $this->driver->findElement(
+                WebDriverBy::xpath(
+                    $xpath
+                )
+            );
+            echo($element->getAttribute("src"));
+            return $element->getAttribute("src");
+        }
+        echo('not found'. $xpath);
+        return null;
+     
+    }
 
     private function selenium(){
-        // selenium
-        $element = $this->driver->findElement(WebDriverBy::cssSelector('.typeahead_input'));
-        // 検索Boxにキーワードを入力して
-        $element->sendKeys('Chiang Mai');
-        // 検索実行
+        $search_box = $this->driver->findElement(WebDriverBy::cssSelector('.typeahead_input'));
+        $search_box->sendKeys('Chiang Mai');
 
-        $element = $this->driver->findElement(WebDriverBy::cssSelector('#SUBMIT_RESTAURANTS'));
-        $element->click(); 
+        $submit_button = $this->driver->findElement(WebDriverBy::cssSelector('#SUBMIT_RESTAURANTS'));
+        $submit_button->click(); 
 
         $this->driver->wait(10, 500)->until(
             WebDriverExpectedCondition::titleContains('BEST Restaurants')
         );
 
-        $this->clickWithXpath(
-            "//div[@id='jfy_filter_bar_establishmentTypeFilters']/div[2]/div/div/a"
-        ); 
-        $this->clickWithXpath(
-            "//div[@id='jfy_filter_bar_price']/div[2]/div[1]/div"
-        );
-        $this->clickWithXpath(
-            "//div[@id='jfy_filter_bar_price']/div[2]/div[2]/div"
-        );
+        $restaurant_checkbox = "//div[@id='jfy_filter_bar_establishmentTypeFilters']/div[2]/div/div/a";
+        $cheap = "//div[@id='jfy_filter_bar_price']/div[2]/div[1]/div";
+        $midrange = "//div[@id='jfy_filter_bar_price']/div[2]/div[1]/div";
+
+        $this->clickWithXpath($restaurant_checkbox); 
+        $this->clickWithXpath($restaurant_checkbox); 
+        
+        $this->clickWithXpath($cheap);
+        $this->clickWithXpath($midrange);
 
 
         $this->driver->wait(10, 500)->until(
@@ -108,8 +124,31 @@ class ScrapeRestaurants extends Command
                 )
             )
         );
-        for ($i = 1; $i <= 30; $i++) {
+        $this->driver->manage()->timeouts()->implicitlyWait(0.5);
+        for ($i = 1; $i <= 20; $i++) {
             $this->clickWithXpath("//div[".$i."]/div/a/div/img");
+        }
+        
+        $current_handle = $this->driver->getWindowHandles();
+        $tabs = array_slice($current_handle, 1);
+        foreach ($tabs as &$tab) {
+            $this->driver->switchTo()->window($tab);
+            $price_xpath="//div[contains(@class,'restaurants-detail-overview-cards-DetailsSectionOverviewCard__detailCard')]/div[2]/div/div[2]";
+            $map_xpath= "//div[contains(@class,'restaurants-detail-overview-cards-LocationOverviewCard__cardColumn')]/span/div/span/img";
+            $xpath_web = WebDriverBy::xpath($price_xpath);
+            $price=null;
+            $latitude=null;
+            $longitude=null;
+            if( count($this->driver->findElements($xpath_web)) > 0 ) {
+                echo('ho!!');
+                $element=$this->driver->findElement($xpath_web);
+                $price = $element->getText();
+                $mapURL = $this->getSrcWithXpath($map_xpath);
+                echo($element->getText());
+                echo($mapURL);
+
+            }
+
         }
                 
     }
